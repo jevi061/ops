dd52ade9cafe6679f5b4118da726fef75e252ff1
jevi061 <2843004288@qq.com>
2024-05-08 20:23:34 +0800

    fix: remove useless code

--- cmd/run.go
+++ cmd/run.go
@@ -32,46 +32,6 @@ func NewRunCmd() *cobra.Command {
 				fmt.Fprintln(os.Stderr, err)
 				os.Exit(1)
 			}
-			// remoteTaskRunners := make([]taskrunner.TaskRunner, 0)
-			// if conf.Servers != nil && len(conf.Servers.Names) > 0 {
-			// 	remoteTaskRunners = o.PrepareTaskConnectors(conf.Servers.Names, tag)
-			// }
-			// taskRuns := make([]taskrunner.TaskRun, 0)
-			// for _, taskName := range args {
-			// 	if runs, err := o.PrepareTaskRuns(taskName, remoteTaskRunners); err != nil {
-			// 		fmt.Fprintln(os.Stderr, err)
-			// 		os.Exit(1)
-			// 	} else {
-			// 		taskRuns = append(taskRuns, runs...)
-			// 	}
-			// }
-			// runners := o.CollectTaskRunners(taskRuns)
-			// if err := o.ConnectTaskRunners(runners); err != nil {
-			// 	fmt.Fprintln(os.Stderr, fmt.Sprintf("CONNECT ERROR(%s):", err.Host), err)
-			// 	os.Exit(1)
-			// }
-			// defer o.CloseTaskRunners(runners)
-			// o.SetTaskRunnersRunningMode(runners, debug)
-			// if debug {
-			// 	o.AlignAndColorTaskRunnersPromets(runners)
-			// }
-			// // relay signals to runners
-			// signals := make(chan os.Signal, 1)
-			// signal.Notify(signals, os.Interrupt)
-			// go func() {
-			// 	if err := o.RelaySignals(runners, signals); err != nil {
-			// 		fmt.Fprintln(os.Stderr, "RUN ERROR:", err)
-			// 		os.Exit(1)
-			// 	}
-			// }()
-			// defer func() {
-			// 	signal.Stop(signals)
-			// 	close(signals)
-			// }()
-			// if err := o.Execute(taskRuns); err != nil {
-			// 	fmt.Fprintln(os.Stderr, "TASK ERROR:", err)
-			// 	os.Exit(1)
-			// }
 		},
 	}
 	runCmd.PersistentFlags().StringVarP(&tag, "tag", "t", "", "server tag")
